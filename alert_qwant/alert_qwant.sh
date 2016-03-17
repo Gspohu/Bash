b455b802a60d0afbb51a5c4e18df98379e2751c2
gspohu <pohuvalentin@gmail.com>
2016-03-17 17:18:07 +0100

    Correction de l'ajout du système de vérification de MAJ

--- alert_qwant/alert_qwant.sh
+++ alert_qwant/alert_qwant.sh
@@ -55,12 +55,12 @@ then
 	#Option de mise à jour
         if [ $1 = "--upgrade" ]
         then
-                wget -q -P /srv/scripts/ https://github.com/Gspohu/Bash/raw/master/alert_qwant/alert_qwant.sh.tmp
-                diff_maj=$(diff alert_qwant.sh.tmp alert_qwant.sh)
+                wget -q -P /tmp/ https://github.com/Gspohu/Bash/raw/master/alert_qwant/alert_qwant.sh
+                diff_maj=$(diff /srv/scripts/alert_qwant.sh /tmp/alert_qwant.sh)
 		if [ "$diff_maj" != "" ]
 		then
 			rm /srv/scripts/alert_qwant.sh
-			mv /srv/scripts/alert_qwant.sh.tmp /srv/scripts/alert_qwant.sh
+			mv /tmp/alert_qwant.sh /srv/scripts/alert_qwant.sh
 			echo "Une mise à jour est disponible, elle à été téléchargé, alert_qwant est à jour " >> /srv/scripts/alert_qwant.log
 		else
 			echo "Aucune mise à jour disponible" >> /srv/scripts/alert_qwant.log
