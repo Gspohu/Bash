eb3c5061cb3e4d5c5cd1798bd9caddf7bbf42e66
gspohu <pohuvalentin@gmail.com>
2016-04-12 13:17:30 +0200

    Savegarde des liens via GET

--- alert_qwant/alert_qwant.sh
+++ alert_qwant/alert_qwant.sh
@@ -368,8 +368,18 @@ then
 	done
 
 	cat BDD_veille.mef | sed s/'\n'/'<br>'/g > BDD_veilleMEF.tmp
-	cat BDD_veilleMEF.tmp | sed s/'<a href'/'<br><a href'/g > BDD_veilleMEF2.tmp
-	cat BDD_veilleMEF2.tmp | sed s/'<\/a>'/'<\/a><a href="https:\/\/cairn-devices.eu" ><img src="https:\/\/raw.githubusercontent.com\/Gspohu\/Bash\/master\/alert_qwant\/ico_save.png" width="15"  alt="icon_save" \/><\/a>'/g > BDD_veille.mef
+	#cat BDD_veilleMEF.tmp | sed s/'<a href'/'<br><a href'/g > BDD_veilleMEF2.tmp
+	echo "" >  BDD_veille.mef
+	cat BDD_veilleMEF.tmp | while read line # Lecture des liens ligne par ligne pour ajouter le paramètre en URL
+	do
+		if [ "${line:0:4}" = "<\/a>" ]
+		then
+			link=$(echo $line | cut -d '"' -f1)
+			echo $line | sed s/'<\/a>'/'   <\/a><a href="https:\/\/cairn-devices.eu/save_link?link=$link" ><img src="https:\/\/raw.githubusercontent.com\/Gspohu\/Bash\/master\/alert_qwant\/ico_save.png" width="17"  alt="icon_save" \/><\/a>'/g >> BDD_veille.mef
+		else
+			echo $link >> BDD_veille.mef
+		fi
+	done
 
 	echo '<br/><br/><br/><br/><center><font color="grey" size="1pt"> Powered by <img src="https://raw.githubusercontent.com/Gspohu/Bash/master/alert_qwant/Qwant_lite_logo.jpg" width="80"  alt="Logo_qwant_lite" /><br/>Le logo de Qwant et le logo de Bash sont la propriété de leur auteurs respectif. En cas de réclamation ou de problème me contacter sur https://github.com/Gspohu</font></center>' >> BDD_veille.mef
 	echo '</body>' >> BDD_veille.mef
